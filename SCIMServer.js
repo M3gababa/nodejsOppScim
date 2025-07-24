ab43a7b5e83bd2000d1435fb01364f893e729a5a
M3gababa <baptiste.duvey@gmail.com>
2025-07-24 19:02:25 +0200

    Use .env file but dont commit

--- SCIMServer.js
+++ SCIMServer.js
@@ -4,35 +4,21 @@ const path = require('path');
 const out = require('./core/logs');
 
 global.appRoot = path.resolve(__dirname);
+const port = process.env.PORT || 3000;
 
-// Sqlite
-const initSQL = require('./core/initDatabase');
-initSQL();
-
-// Now the app
+// Define express app
 const app = express();
-const port = process.env.PORT || 3000;
-
 app.use(bodyParser.json());
-
-// --- Default Endpoints ---
-app.get('/', (req, res) => {
-    out.log("INFO", "GET", "Got request: " + req.url);   
-    let txt = "&#9940; This is not suitable for PRODUCTION &#9940; <br/>";
-    res.send(txt);
-});
-
-app.get('/scim/', (req, res) => {
-    out.log("INFO", "GET", "Got request: " + req.url);
-    let txt = "&#9940; This is not suitable for PRODUCTION &#9940; <br/>";
-    txt += "This aims to be a test for the BANNER database import and is not suitable for PRODUCTION.<br/>";
-    txt += "This endpoint specify that this is a SCIM connector";
-    res.send(txt);
-});
 
 // --- SCIM Endpoints ---
 const scimRoutes = require('./core/routes/scimRoutes');
 app.use('/scim/v2', scimRoutes);
+
+// Undefined endpoints
+app.all('*', (req, res) => {
+    out.log("INFO", req.method, "Got request: " + req.url);
+    res.status(404).send("<h1>Page not found ! </h1><br> <h2>We're sorry, we couldn't find the page you requested.</h2>");
+});
 
 app.listen(port, () => {
     out.log("INFO", "START", "Service Started");

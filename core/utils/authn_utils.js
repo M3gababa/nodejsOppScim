1a0d9a10b95c2956f336e68937745b41f8aab73e
M3gababa <baptiste.duvey@gmail.com>
2025-07-24 23:20:12 +0200

    Remove log

--- core/utils/authn_utils.js
+++ core/utils/authn_utils.js
@@ -11,10 +11,9 @@ const authenticate = (req, res, next) => {
     const user = auth(req);
   
     if (!user || creds.login !== user.name || creds.pwd !== user.pass) {
-        res.setHeader('WWW-Authenticate', 'Basic realm="My Realm"');
+        res.setHeader('WWW-Authenticate', 'Basic');
         res.status(401).send({ error: 'Authentication required' });
     } else {
-            console.log(user.name + " " + user.pass);
         req.user = user;
         next();
     }

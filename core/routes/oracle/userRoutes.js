c0a82680f94ede7714570970a2818d1997fb7bc8
M3gababa <baptiste.duvey@gmail.com>
2025-07-26 01:18:26 +0200

    Fix Oracle Users functions

--- core/routes/oracle/userRoutes.js
+++ core/routes/oracle/userRoutes.js
@@ -14,16 +14,17 @@ router.get('/', authenticate, async (req, res) => {
     let connection;
     try {
         connection = await getConnection();
-        const query = `SELECT * FROM COMPTES WHERE id = :id`;
-        const result = await executeQuery(connection, query, [userId]);
+        const query = `select * from COMPTES`;
+        const result = await executeQuery(connection, query);
+        const users = result.rows.map(row => scimUtils.createScimUserFromTableRow(row));
+
+        const jsonResult = {
+            schemas: ["urn:ietf:params:scim:api:messages:2.0:ListResponse"],
+            totalResults: users.length,
+            Resources: users
+        };
         
-        if (result.rows.length > 0) {
-            const jsonResult = createScimUserFromTableRow(result.rows[0]);
-            res.json(jsonResult);
-        } else {
-            out.log("WARNING", "GET", `User ${userId} not found`);
-            res.status(404).json({ schemas: ["urn:ietf:params:scim:api:messages:2.0:Error"], status: "404", detail: `User ${userId} not found` });
-        }
+        res.json(jsonResult);
     } catch (err) {
         out.log("ERROR", "GET", err);
         res.status(500).json({ schemas: ["urn:ietf:params:scim:api:messages:2.0:Error"], status: "500", detail: `Error retrieving user ${userId}` });
@@ -38,8 +39,14 @@ router.get('/:id', authenticate, async (req, res) => {
     out.log("INFO", "GET", "Got request: " + req.originalUrl);
     
     const userId = req.params.id;
+    let connection;
     try {
-        if (userId) {
+        connection = await getConnection();
+        
+        const query = `select * from COMTPES WHERE id = :id`;
+        const result = await executeQuery(connection, query, [userId]);
+        if (result.rows.length > 0) {
+            const jsonResult = scimUtils.createScimUserFromTableRow(result.rows[0]);
             res.json(jsonResult);
         } else {
             out.log("WARNING", "GET", `User ${userId} not found`);
